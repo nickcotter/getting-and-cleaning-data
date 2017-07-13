fce6b3e42c1c75dd8630f466021f58e0518421be
Nick Cotter <nick@extropy.net>
2017-07-13 21:37:06 +0100

    more extraction

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,27 @@
 require("data.table")
 require("reshape2")
 
-#Merges the training and the test sets to create one data set.
+# 1. Merges the training and the test sets to create one data set.
 
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
 features <- read.table("./UCI HAR Dataset/features.txt")[,2]
 
-mean_and_std_features <- grepl("mean|std", features)
+training_set <- read.table("./UCI HAR Dataset/train/X_train.txt")
+training_labels <- read.table("./UCI HAR Dataset/train/y_train.txt")
+test_set <- read.table("./UCI HAR Dataset/test/X_test.txt")
+test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt")
 
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-names(X_test) = features
 
-X_test = X_test[,mean_and_std_features]
+names(test_set) = features
 
-#Extracts only the measurements on the mean and standard deviation for each measurement.
-#Uses descriptive activity names to name the activities in the data set
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-#Appropriately labels the data set with descriptive variable names.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+mean_and_std_features <- grepl("mean|std", features)
+test_set = test_set[,mean_and_std_features]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# 5. Appropriately labels the data set with descriptive variable names.
