596105728ebe135d4a6930d0163df7d85896ca7d
Nick Cotter <nick@extropy.net>
2017-07-13 20:58:50 +0100

    only download and unzip if not already performed

--- setup_raw_data.R
+++ setup_raw_data.R
@@ -1,2 +1,7 @@
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Dataset.zip")
-unzip("Dataset.zip")
+if(!file.exists("Dataset.zip")) {
+        download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Dataset.zip")
+}
+
+if(!file.exists("UCI HAR Dataset")) {
+        unzip("Dataset.zip")    
+}
